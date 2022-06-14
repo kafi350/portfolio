be0df57fb538b474eb3c9d2aec9cf60639e42a96
kafi350 <kafiabdullahil96@gmail.com>
2022-06-14 15:19:48 +0200

    Update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,9 +3,17 @@ import "./header.css"
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+       <header>
+           <div className="container header__container">
+               <h5>Hi I'm</h5>
+               <h1>Abdullahil Kafi</h1>
+               <h5 className="text-light">IT Project Staff (Software Engineer)</h5>
+
+           </div>
+
+
+
+       </header>
     )
 }
 export default Header
